057509e2f957d10d3708fd6612963b507485d927
Juan David Rivera Marin <dazeddeath71@hotmail.com>
2025-10-28 23:04:37 -0400

    Button & Funtion

--- src/main/java/com/myproject/laboratorio1/Laboratorio1.java
+++ src/main/java/com/myproject/laboratorio1/Laboratorio1.java
@@ -186,7 +186,7 @@ public class Laboratorio1 extends javax.swing.JFrame {
         });
         jPanel1.add(jToggleButton4);
 
-        jButton1.setText("jButton1");
+        jButton1.setText("Graficar");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
